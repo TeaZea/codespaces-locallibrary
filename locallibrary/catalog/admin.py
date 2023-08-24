51a91f26fb81fccae30a1d2d65e8df9b77f287d5
Stefano Sena <91676539+TeaZea@users.noreply.github.com>
2023-08-24 20:04:51 +0000

    Added inlines of Book to Author /admin/

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -5,11 +5,16 @@ from .models import Author, Genre, Book, BookInstance, Language
 # admin.site.register(Author)
 # admin.site.register(BookInstance)
 
+class BookInline(admin.TabularInline):
+    model = Book
+
 # Define the admin class
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
 
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')] # Changes the view of the vields in admin
+
+    inlines = [BookInline]
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
